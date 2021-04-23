26f10f1d882f469c2311cf578d06e9046e08b25a
kac-kon <kacper.kontny@gmail.com>
2021-04-23 16:18:28 +0200

    weather display update

--- handler.py
+++ handler.py
@@ -238,4 +238,4 @@ if __name__ == "__main__":
     api.add_resource(ForecastHourly, "/forecast/hourly")
     api.add_resource(LCD, "/LCD/<int:mode>")
 
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
